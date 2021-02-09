bcb3bedf53e7a0cf3f7b0f7c27f7b257b95e5d82
Rafael Martins <rafaeldcmartins@gmail.com>
2021-02-09 15:11:43 -0300

    Typing Strapi posts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/app';
 
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
