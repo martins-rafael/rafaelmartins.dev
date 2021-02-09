26df39bbe916407e21c8abaf06531448aecd9eb8
Rafael Martins <rafaeldcmartins@gmail.com>
2021-02-09 16:10:45 -0300

    NextJS SSG (Static Site Generation)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <h1>Lorem ipsum dolor sit amet.</h1>;
+import { GetStaticProps } from 'next';
+
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://protected-eyrie-31544.herokuapp.com/posts',
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts;
+};
+
+type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+    // revalidate: 5,
+  };
+};
