8e338130ba4880ef1c4bef5cc8dd2be5318fdd73
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-02 12:44:04 -0300

    Add production url

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://protected-eyrie-31544.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Rafael Martins';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://kind-jackson-4d0210.netlify.app';
