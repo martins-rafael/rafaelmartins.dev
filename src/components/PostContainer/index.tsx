a75bc02567c75891654f34fa1bdbddc5306ec9b9
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-30 19:50:14 -0300

    Add Prism syntax highlighter

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+import Prism from 'prismjs';
+
+import { PrismWrapper } from './prism-dracula';
 import { Container } from './styles';
 
 type PostContainerProps = {
   content: string;
 };
 
-const PostContainer = ({ content }: PostContainerProps) => (
-  <Container dangerouslySetInnerHTML={{ __html: content }} />
-);
+const PostContainer = ({ content }: PostContainerProps) => {
+  useEffect(() => {
+    setTimeout(() => Prism.highlightAll(), 0);
+  }, []);
+
+  return (
+    <PrismWrapper>
+      <Container dangerouslySetInnerHTML={{ __html: content }} />
+    </PrismWrapper>
+  );
+};
 
 export default PostContainer;
--- src/components/PostContainer/styles.ts
+++ src/components/PostContainer/styles.ts
@@ -1,8 +1,6 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.article`
-  margin-top: 2rem;
-
   img {
     max-width: 100%;
   }
@@ -15,20 +13,5 @@ export const Container = styled.article`
   ul,
   ol {
     margin: ${({ theme }) => theme.spacings.medium};
-  }
-
-  pre {
-    ${({ theme }) => css`
-      margin: ${theme.spacings.large} 0;
-      padding: ${theme.spacings.large};
-      width: 100%;
-      font-size: ${theme.font.sizes.medium};
-      line-height: 1.5;
-      color: ${theme.colors.text};
-      background: ${theme.colors.lightGray};
-      border-radius: 1rem;
-      box-shadow: 0.5rem 0.5rem 1.5rem -0.5rem rgba(0, 0, 0, 0.1);
-      overflow-x: auto;
-    `}
   }
 `;
