1d430e3e6af9143d245a7a02eb612a32fc1771fb
Rafael Martins <rafaeldcmartins@gmail.com>
2021-04-28 22:11:39 -0300

    Update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,50 @@
 import Link from 'next/link';
-import { Container } from './styles';
+import { useState } from 'react';
+import { CgMenuRightAlt, CgClose } from 'react-icons/cg';
+import { Container, HeaderContent, LinksContainer } from './styles';
 
-const Header = () => (
-  <Container>
-    <Link href="/">
-      <a>
-        <span>{'{'}</span>RafaMartins<span>{'}'}</span>
-      </a>
-    </Link>
-  </Container>
-);
+const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
+  return (
+    <Container>
+      <HeaderContent>
+        <Link href="/">
+          <a>
+            <span>{'{'}</span>R<span>{'}'}</span>
+          </a>
+        </Link>
+
+        <button onClick={handleMenu}>
+          {menuIsOpen ? <CgClose /> : <CgMenuRightAlt />}
+        </button>
+
+        <LinksContainer active={menuIsOpen}>
+          <li>
+            <Link href="/about">
+              <a>Sobre</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/projects">
+              <a>Portfólio</a>
+            </Link>
+          </li>
+        </LinksContainer>
+      </HeaderContent>
+    </Container>
+  );
+};
 
 export default Header;
