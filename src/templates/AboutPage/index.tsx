08f19d749c193ac4cda9c257283751050ecba661
Rafael Martins <rafaeldcmartins@gmail.com>
2021-04-28 22:09:22 -0300

    PostPage and ProjectsPage Templates

---
+++ src/templates/AboutPage/index.tsx
@@ -0,0 +1,9 @@
+import BaseTemplate from '../BaseTemplate';
+
+const AboutPage = () => (
+  <BaseTemplate>
+    <h1>Sobre</h1>
+  </BaseTemplate>
+);
+
+export default AboutPage;
---
+++ src/templates/ProjectsPage/index.tsx
@@ -0,0 +1,9 @@
+import BaseTemplate from '../BaseTemplate';
+
+const ProjectsPage = () => (
+  <BaseTemplate>
+    <h1>Portfolio</h1>
+  </BaseTemplate>
+);
+
+export default ProjectsPage;
