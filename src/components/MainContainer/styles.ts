4db71a29f49e37501e1cd54582a1a33df94885a8
Rafael Martins <rafaeldcmartins@gmail.com>
2021-04-28 22:02:16 -0300

    Dimensions adjustments

--- src/components/MainContainer/styles.ts
+++ src/components/MainContainer/styles.ts
@@ -1,22 +1,21 @@
 import styled, { css, keyframes } from 'styled-components';
 
-const appearFromLeft = keyframes`
+const appear = keyframes`
   from {
     opacity: 0;
-    transform: translateX(-50px);
   }
   to {
     opacity: 1;
-    transform: translateX(0);
   }
 `;
 
 export const Container = styled.main`
   ${({ theme }) => css`
-    margin: 0 auto;
+    margin: 15rem auto 0;
     padding: 1.6rem;
+    width: 100%;
     max-width: 80rem;
     font-size: ${theme.font.sizes.medium};
-    animation: ${appearFromLeft} 1s;
+    animation: ${appear} 2s;
   `}
 `;
