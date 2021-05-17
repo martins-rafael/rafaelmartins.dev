289110e0729bae93656255e41e621f31c4c0ae0a
Rafael Martins <rafaeldcmartins@gmail.com>
2021-05-17 10:39:27 -0300

    Add AOS

--- src/templates/BlogPage/index.tsx
+++ src/templates/BlogPage/index.tsx
@@ -1,4 +1,6 @@
-import Head from 'next/head';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import { PostData } from '../../domain/posts/post';
 import { SITE_NAME } from '../../config/app-config';
@@ -11,16 +13,22 @@ type BlogPageProps = {
   posts: PostData[];
 };
 
-const BlogPage = ({ posts }: BlogPageProps) => (
-  <>
+const BlogPage = ({ posts }: BlogPageProps) => {
+  useEffect(() => {
+    AOS.init({ duration: 700 });
+  }, []);
+
+  return (
+    <>
       <SEO title={`Blog | ${SITE_NAME}`} />
 
-    <AnimationContainer>
-      {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
-    </AnimationContainer>
-  </>
-);
+      <AnimationContainer animation="appearFromAbove">
+        {posts.map((post) => (
+          <PostCard effect="fade-up" key={post.id} post={post} />
+        ))}
+      </AnimationContainer>
+    </>
+  );
+};
 
 export default BlogPage;
