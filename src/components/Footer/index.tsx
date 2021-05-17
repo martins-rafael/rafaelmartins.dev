e7326e1bee2b6ee1bf662136484032641d5b2b99
Rafael Martins <rafaeldcmartins@gmail.com>
2021-05-17 10:25:04 -0300

    Add scroll to top button

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,41 +1,52 @@
 import { SiGithub } from 'react-icons/si';
 import { SiLinkedin } from 'react-icons/si';
 import { FiMail } from 'react-icons/fi';
+import { IoIosArrowUp } from 'react-icons/io';
 
 import { SITE_NAME } from '../../config/app-config';
 
 import { Container } from './styles';
 
-const Footer = () => (
-  <Container>
-    <div>
-      © 2021 - Feito com 💜 by <span>{SITE_NAME}</span>
-    </div>
+const Footer = () => {
+  const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-    <div>
-      <a title="Email" href="mailto:rafaeldcmartins@gmail.com">
-        <FiMail size={16} />
-      </a>
+  return (
+    <Container>
+      <div>
+        © 2021 - Feito com 💜 by <span>{SITE_NAME}</span>
+      </div>
 
-      <a
-        title="LinkedIn"
-        href="https://www.linkedin.com/in/rafaeldcmartins/"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <SiLinkedin size={16} />
-      </a>
+      <div>
+        <a title="Email" href="mailto:rafaeldcmartins@gmail.com">
+          <FiMail size={20} />
+        </a>
 
-      <a
-        title="GitHub"
-        href="https://github.com/martins-rafael"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <SiGithub size={16} />
-      </a>
-    </div>
-  </Container>
-);
+        <a
+          title="LinkedIn"
+          href="https://www.linkedin.com/in/rafaeldcmartins/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <SiLinkedin size={20} />
+        </a>
+
+        <a
+          title="GitHub"
+          href="https://github.com/martins-rafael"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <SiGithub size={20} />
+        </a>
+
+        <a title="Ir para o topo" onClick={backToTop}>
+          <IoIosArrowUp size={20} />
+        </a>
+      </div>
+    </Container>
+  );
+};
 
 export default Footer;
