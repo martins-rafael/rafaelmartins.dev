a860ed36efcf90fbec8533c7e653293059d85d36
Rafael Martins <rafaeldcmartins@gmail.com>
2021-05-17 10:45:26 -0300

    Update HomePage

--- src/templates/HomePage/index.tsx
+++ src/templates/HomePage/index.tsx
@@ -1,19 +1,17 @@
-import Head from 'next/head';
 import Image from 'next/image';
 
 import SEO from '../../components/SEO';
 import AnimationContainer from '../../components/AnimationContainer';
+import SkillsContainer from '../../components/SkillsContainer';
 
-import { Container } from './styles';
+import { WelcomeContainer, DescriptionContainer } from './styles';
 
 const HomePage = () => (
   <>
     <SEO title="Rafael Martins - Desenvolvedor Frontend" />
 
-    <ParticlesBackground />
-
-    <AnimationContainer>
-      <Container>
+    <AnimationContainer animation="appearFromAbove">
+      <WelcomeContainer>
         <div>
           <span>
             <Image
@@ -40,7 +38,57 @@ const HomePage = () => (
           height={320}
           layout="responsive"
         />
-      </Container>
+      </WelcomeContainer>
+
+      <DescriptionContainer>
+        <h2>
+          <span>&gt;</span> SOBRE
+        </h2>
+
+        <p>
+          Web developer autodidata, atualmente estou me aprofundando no
+          desenvolvimento Frontend, com foco na construção de interfaces
+          responsivas, escrevendo código limpo e eficiente. Apaixonado por
+          tecnologia, livros, games e música.
+        </p>
+
+        <p>
+          Minhas techs favoritas sâo: HTML5, CSS(SASS ❤), JavaScript e Next.js.
+        </p>
+
+        <p>
+          Você pode entrar em contato comigo através do{' '}
+          <a
+            title="LinkedIn"
+            href="https://www.linkedin.com/in/rafaeldcmartins/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            LinkedIn
+          </a>
+          , ver o que ando desenvolvendo no{' '}
+          <a
+            title="GitHub"
+            href="https://github.com/martins-rafael"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>{' '}
+          e mandar um oi no{' '}
+          <a
+            title="Discord"
+            href="http://discordapp.com/users/681293490428313641"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Discord (Rafa Martins#1783)
+          </a>
+          .
+        </p>
+      </DescriptionContainer>
+
+      <SkillsContainer />
     </AnimationContainer>
   </>
 );
