89f5984cc75d920e271d3f2b1f918fbf50daaf13
Rafael Martins <rafaeldcmartins@gmail.com>
2021-07-14 21:31:14 -0300

    Update babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   "presets": ["next/babel"],
-  "plugins": [["styled-components", { "ssr": true }]]
+  "plugins": [["styled-components", { "ssr": true }]],
+  "env": {
+    "development": {
+      "plugins": [
+        [
+          "prismjs",
+          {
+            "languages": "all",
+            "plugins": ["keep-markup"],
+            "css": true
+          }
+        ]
+      ]
+    }
+  }
 }
