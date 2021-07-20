d88dd5b449a07256656e40702a32a4fa50fedd07
Rafael Martins <rafaeldcmartins@gmail.com>
2021-07-20 12:42:45 -0300

    Upgrading Next.js from version 10 to 11

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     node: true,
   },
   extends: [
+    'next',
+    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
