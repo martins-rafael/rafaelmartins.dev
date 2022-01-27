1c7a271519b5e54039ffc58ebb5576001c58eb0c
Rafael Martins <rafaeldcmartins@gmail.com>
2022-01-26 22:31:24 -0300

    Add NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 import BaseTemplate from 'templates/Base';
 
@@ -10,6 +11,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+
+      <NextNProgress
+        color={theme.colors.green}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
+
       <BaseTemplate>
         <Component {...pageProps} />
       </BaseTemplate>
